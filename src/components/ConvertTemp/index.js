9acdf2bfea5591b75cc4c60e00384e63c576fc62
Awais Muzaffar <awaismuzaffar@awaiss-mbp.home>
2020-03-29 12:05:26 +0100

    Tidy up formatting

--- src/components/ConvertTemp/index.js
+++ src/components/ConvertTemp/index.js
@@ -3,18 +3,18 @@ import { LABEL_CELSIUS, LABEL_FARENHEIGHT } from '../../constants';
 
 function ConvertTemp(props) {
 
-  const [temp, setTemp] = useState({c: null, f: null});
+  const [temp, setTemp] = useState({ c: null, f: null });
 
   function toCelsius(k) {
 
-    const converted = (k - 273.15);
+    const converted = k - 273.15;
 
     return (Math.round(converted * 10) / 10);
   }
 
   function toFarenheight(k) {
 
-    const converted = ((k - 273.15)*1.8)+32;
+    const converted = ((k - 273.15) * 1.8) + 32;
 
     return (Math.round(converted * 10) / 10);
   }
@@ -26,7 +26,7 @@ function ConvertTemp(props) {
     })
   }, [props.kelvin, props.showCelsius]);
   
-return props.showCelsius ?  <span>{temp.c}{ LABEL_CELSIUS }</span> : <span>{ temp.f }{ LABEL_FARENHEIGHT }</span>;
+  return props.showCelsius ? <span>{ temp.c }{ LABEL_CELSIUS }</span> : <span>{ temp.f }{ LABEL_FARENHEIGHT }</span>;
 }
 
 export default ConvertTemp;
