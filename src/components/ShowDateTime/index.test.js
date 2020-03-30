971c5527f118a6bea8057a7ff12855672d06e9ef
Awais Muzaffar <awaismuzaffar@awaiss-mbp.home>
2020-03-30 13:05:14 +0100

    Put tests in describe block

--- src/components/ShowDateTime/index.test.js
+++ src/components/ShowDateTime/index.test.js
@@ -1,30 +1,33 @@
 import React from 'react';
 import { render, unmountComponentAtNode } from "react-dom";
-import  ShowDateTime from './index';
 import { act } from "react-dom/test-utils";
+import  ShowDateTime from '.';
 
-let container = null;
+describe('ShowDateTime', () => {
+  
+  let container = null;
 
-beforeEach(() => {
-  container = document.createElement("div");
-  document.body.appendChild(container);
-});
-
-afterEach(() => {
-  unmountComponentAtNode(container);
-  container.remove();
-  container = null;
-});
-
-it("renders", () => {
-  render(<ShowDateTime />, container);
-});
-
-it("should show the formatted date from a unix timestamp", () => {
-
-  act(() => {
-    render(<ShowDateTime dt="1485799200" />, container);
+  beforeEach(() => {
+    container = document.createElement("div");
+    document.body.appendChild(container);
   });
   
-  expect(container.textContent).toBe("January 30th 2017, 6:00pm");
+  afterEach(() => {
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+  });
+  
+  it("renders", () => {
+    render(<ShowDateTime />, container);
+  });
+  
+  it("should show the formatted date from a unix timestamp", () => {
+  
+    act(() => {
+      render(<ShowDateTime dt="1485799200" />, container);
+    });
+    
+    expect(container.textContent).toBe("January 30th 2017, 6:00pm");
+  });
 });
